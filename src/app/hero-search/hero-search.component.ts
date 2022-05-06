a63fc66954249db7b06bbb59700f7ad0d3428fd6
LourdesFayos <104846119+LourdesFayos@users.noreply.github.com>
2022-05-06 14:17:02 +0200

    actualizado 06/05

---
+++ src/app/hero-search/hero-search.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-search',
+  templateUrl: './hero-search.component.html',
+  styleUrls: ['./hero-search.component.css']
+})
+export class HeroSearchComponent implements OnInit {
+  heroes$!: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private heroService: HeroService) { }
+
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+  /* debounceTime(300) -> esperamos 300ms despues de cada pulsación de teclas
+    antes de tener en cuenta el termino.
+
+    distinctUntilChanged() -> asegura que se envie una peticion solo si el texto
+    del filtro ha cambiado.
+    
+    switchMap((term: string) => this.heroService.searchHeroes(term)) -> cambia a un
+    nuevo Observable de busqueda cada vez que el termino cambia. 
+  */ 
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(debounceTime(300), 
+    distinctUntilChanged(), 
+    switchMap((term: string) => this.heroService.searchHeroes(term))
+    );
+  }
+
+}
