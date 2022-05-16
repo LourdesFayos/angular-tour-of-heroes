e74e16a77af97b6f1896ebd617ac143d623fea2d
LourdesFayos <104846119+LourdesFayos@users.noreply.github.com>
2022-05-16 18:18:58 +0200

    template-driven form

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [ 
-    { id: 11, name: 'Dr Nice' }, 
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
+    { id: 11, name: 'Dr Nice', power: 'Super Hot' }, 
+    { id: 12, name: 'Narco', power: 'Really Smart' },
+    { id: 13, name: 'Bombasto', power: 'Weather Changer' },
+    { id: 14, name: 'Celeritas', power: 'Super Flexible' },
+    { id: 15, name: 'Magneta', power: 'Super Hot' },
+    { id: 16, name: 'RubberMan', power: 'Super Flexible' },
+    { id: 17, name: 'Dynama', power: 'Really Smart' },
+    { id: 18, name: 'Dr IQ', power: 'Super Flexible' },
+    { id: 19, name: 'Magma', power: 'Super Hot' },
+    { id: 20, name: 'Tornado', power: 'Weather Changer' }
 ];
